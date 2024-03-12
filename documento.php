918ce78e89260684d5b79642d4852b5606b86fc3
flwbielzin <134297238+flwbielzin@users.noreply.github.com>
2024-03-11 23:33:57 -0300

    correção

--- documento.php
+++ documento.php
@@ -1,20 +1,18 @@
 <?php
+$nome = addslashes($_POST['nome']);
+$email = addslashes($_POST['email']);
+$celular = addslashes($_POST['celular']);
 
-    $nome = addslashes($_POST['nome']);
-    $email = addslashes($_POST['email']);
-    $celular = addslashes($_POST['celular']);
+$para = "msgabryelll22oliveira@gmail.com";
+$assunto = "Coleta de dados - Inteliogia";
 
-    $para = "msgabryelll22oliveira@gmail.com";
-    $assunto = "Coleta de dados - Inteliogia";
+$corpo = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Telefone: " . $celular;
 
-    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$celular;
+$cabeca = "From: teste@inteliogia.com" . "\n" . "Reply-to: " . $email . "\n" . "X=Mailer:PHP/" . phpversion();
 
-    $cabeca = "From: teste@inteliogia.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
-
-    if(mail($para,$assunto,$corpo,$cabeca)){
-        echo("E-mail enviado com sucesso!");
-    }else{
-        echo("Houve um erro ao enviar o email!");
-    }
-
+if (mail($para, $assunto, $corpo, $cabeca)) {
+    echo("E-mail enviado com sucesso!");
+} else {
+    echo("Houve um erro ao enviar o email: " . error_get_last()['message']);
+}
 ?>
