7bef4980daa849704e09eee9dde11bf8e80b33c8
flwbielzin <134297238+flwbielzin@users.noreply.github.com>
2024-03-22 21:43:25 -0300

    tentando corrigir erro formulário

--- documento.php
+++ documento.php
@@ -1,18 +1,26 @@
 <?php
-$nome = addslashes($_POST['nome']);
-$email = addslashes($_POST['email']);
-$celular = addslashes($_POST['celular']);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = isset($_POST['nome']) ? addslashes($_POST['nome']) : '';
+    $email = isset($_POST['email']) ? addslashes($_POST['email']) : '';
+    $celular = isset($_POST['celular']) ? addslashes($_POST['celular']) : '';
 
-$para = "msgabryelll22oliveira@gmail.com";
-$assunto = "Coleta de dados - Inteliogia";
+    if (!empty($nome) && !empty($email) && !empty($celular)) {
+        $para = "msgabryelll22oliveira@gmail.com";
+        $assunto = "Coleta de dados - Inteliogia";
 
-$corpo = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Telefone: " . $celular;
+        $corpo = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Telefone: " . $celular;
 
-$cabeca = "From: teste@inteliogia.com" . "\n" . "Reply-to: " . $email . "\n" . "X=Mailer:PHP/" . phpversion();
+        $cabeca = "From: teste@inteliogia.com" . "\n" . "Reply-to: " . $email . "\n" . "X=Mailer:PHP/" . phpversion();
 
-if (mail($para, $assunto, $corpo, $cabeca)) {
-    echo("E-mail enviado com sucesso!");
+        if (mail($para, $assunto, $corpo, $cabeca)) {
+            echo("E-mail enviado com sucesso!");
+        } else {
+            echo("Houve um erro ao enviar o email: " . error_get_last()['message']);
+        }
+    } else {
+        echo "Por favor, preencha todos os campos do formulário.";
+    }
 } else {
-    echo("Houve um erro ao enviar o email: " . error_get_last()['message']);
+    echo "Acesso inválido ao script.";
 }
 ?>
